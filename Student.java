05fe995ebfc4d2b5e1be4723284a2b11fc86089f
Gangadhar <30434269+Gangadharjawali@users.noreply.github.com>
2017-10-04 19:23:28 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,30 @@
+package com.hibernate.example;
+
+
+
+public class Student {
+	private int sid;
+	private String sname;
+	private double smarks;
+
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public double getSmarks() {
+		return smarks;
+	}
+	public void setSmarks(double smarks) {
+		this.smarks = smarks;
+	}
+	
+
+}
