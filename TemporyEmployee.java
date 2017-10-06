17de95bf6568b1b0f9227abd67847668f06e2e74
Gangadhar <30434269+Gangadharjawali@users.noreply.github.com>
2017-10-06 19:28:38 +0530

    Add files via upload

---
+++ TemporyEmployee.java
@@ -0,0 +1,24 @@
+package com.hibernate.example;
+
+public class TemporyEmployee extends Employee {
+	
+	private int workingDays;
+	private String contractorName;
+
+	public int getWorkingDays() {
+		return workingDays;
+	}
+
+	public void setWorkingDays(int workingDays) {
+		this.workingDays = workingDays;
+	}
+
+	public String getContractorName() {
+		return contractorName;
+	}
+
+	public void setContractorName(String contractorName) {
+		this.contractorName = contractorName;
+	}
+
+}
