8a998b69f9a3d65f711d5e439cc3c3a7794b6b18
Gangadhar <30434269+Gangadharjawali@users.noreply.github.com>
2017-10-06 19:28:54 +0530

    Add files via upload

---
+++ PermanentEmployee.java
@@ -0,0 +1,23 @@
+package com.hibernate.example;
+
+public class PermanentEmployee extends Employee {
+	
+	private String designation, department;
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+}
